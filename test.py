5971a5ddf6d7dee89a0572b20d301a130bab3157
chrzyki <christoph@functor-argument.net>
2019-04-09 14:36:21 +0200

    Fixed broken test.

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ from __future__ import unicode_literals
 
 
 def test_valid(cldf_dataset, cldf_logger):
-assert cldf_dataset.validate(log=cldf_logger)
+    assert cldf_dataset.validate(log=cldf_logger)
+
