d3f009eb0a37f7fc2b11879dc32a4771b899acda
chrzyki <christoph@functor-argument.net>
2019-11-11 15:01:43 +0100

    Bugfix release for Zenodo

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ def test_valid(cldf_dataset, cldf_logger):
 def test_forms(cldf_dataset):
     assert len(list(cldf_dataset["FormTable"])) == 21967
     assert any(f["Form"] == "tɑ³⁵pʐɑ³⁵n̥e³⁵" for f in cldf_dataset["FormTable"])
+    assert any(f["Form"] == "vɛ⁵⁵" for f in cldf_dataset["FormTable"])
 
 
 def test_parameters(cldf_dataset):
